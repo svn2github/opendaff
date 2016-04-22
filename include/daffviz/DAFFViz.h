16d3c0c766b19de0e9975b00c6a04369b21e3578
jstienen <jstienen@22735341-649f-4b50-8b46-d246c0249a88>
2016-04-22 08:01:00 +0000

    add: README file with installation guides for Linux and Windows (using CMake)
    git-svn-id: https://svn.code.sf.net/p/opendaff/code/trunk@216 22735341-649f-4b50-8b46-d246c0249a88

--- include/daffviz/DAFFViz.h
+++ include/daffviz/DAFFViz.h
@@ -1,34 +1,36 @@
 /*
-* -------------------------------------------------------------------------------------
-*
-*  OpenDAFF - A free, open source software package for directional audio data
-*  OpenDAFF is distributed under the terms of the GNU Lesser Public License (LGPL)
-*
-*  Copyright (c) Institute of Technical Acoustics, RWTH Aachen University, 2009-2016
-*
-*  ------------------------------------------------------------------------------------
-*
-*  Visit the OpenDAFF homepage: http://www.opendaff.org
-*
-*  ------------------------------------------------------------------------------------
-*
-*  License and warranty notes
-*
-*  OpenDAFF is free software, distributed under the terms of the
-*  GNU Lesser General Public License (LGPL) version 3.
-*  You can redistribute it and/or modify it under the terms of the
-*  GNU Lesser General Public License (LGPL) version 3,
-*  as published by the Free Software Foundation.
-*
-*  This program is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*  See the GNU General Public License for more details.
-*
-*  You should have received a copy of the GNU General Public License
-*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-*/
+ * -------------------------------------------------------------------------------------
+ *
+ *  OpenDAFF - A free, open source software package for directional audio data
+ *  OpenDAFF is distributed under the terms of the GNU Lesser Public License (LGPL)
+ *
+ *  Copyright (c) Institute of Technical Acoustics, RWTH Aachen University, 2009-2016
+ *
+ *  ------------------------------------------------------------------------------------
+ *
+ *  Visit the OpenDAFF homepage: http://www.opendaff.org
+ *
+ *  ------------------------------------------------------------------------------------
+ *
+ *  License and warranty notes
+ *
+ *  OpenDAFF is free software, distributed under the terms of the
+ *  GNU Lesser General Public License (LGPL) version 3.
+ *  You can redistribute it and/or modify it under the terms of the
+ *  GNU Lesser General Public License (LGPL) version 3,
+ *  as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  ------------------------------------------------------------------------------------
+ *
+ */
 
 // Base class
 #include "DAFFVizSGNode.h"
