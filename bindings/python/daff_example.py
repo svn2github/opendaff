5d59a23f5d32988e1c7e90f760166b888ad822b5
aerr <aerr@22735341-649f-4b50-8b46-d246c0249a88>
2018-06-13 09:16:53 +0000

    pydaff.cpp: Added functions to bind cpp-methods to python
    git-svn-id: https://svn.code.sf.net/p/opendaff/code/trunk@370 22735341-649f-4b50-8b46-d246c0249a88

--- bindings/python/daff_example.py
+++ bindings/python/daff_example.py
@@ -4,10 +4,15 @@ sys.path.append( 'dist/Lib/site-packages' )
 
 import daff
 
-d1 = daff.open( '../../matlab/ExampleUnityOmni.ms.v17.daff' )
-d2 = daff.open( '../../matlab/ExampleUnityOmni.ms.v17.daff' )
+d1 = daff.open( 'ExampleUnityOmni.ms.daff' )
+d2 = daff.open( 'ExampleUnityOmni.ms.daff' )
 
+
+index = daff.find_nearest_neighbour_index(d1,1,2,3)
 print( daff.content_type( d1 ) )
+
+
+
 
 daff.close( d2 )
 daff.close( d1 )
--- bindings/python/pydaff.cpp
+++ bindings/python/pydaff.cpp
@@ -44,6 +44,9 @@ static std::map< int, DAFFReader* > g_mReader; //!< Global reader instance map
 static int g_iLastHandle = 0; //!< Stores ID of last used handle identifier
 static PyObject* g_pDAFFError = nullptr; //!< Static pointer to error instance
 
+
+void GetRecordPython(DAFFReader* pReader, int iRecordIndex, PyObject* &listResult);
+
 bool ValidHandle( const int iHandle )
 {
 	if( g_mReader.find( iHandle ) == g_mReader.end() )
@@ -73,7 +76,7 @@ static PyObject* daff_open( PyObject*, PyObject** ppArgs, Py_ssize_t nArgs, PyOb
 
 	delete pReader;
 
-	PyErr_SetString( PyExc_ConnectionError, std::string( "Could not open " + sFilePath ).c_str() );
+	PyErr_SetString( PyExc_ConnectionError, std::string( "Could not iopen " + sFilePath ).c_str() );
 	return NULL;
 }
 
@@ -102,19 +105,227 @@ static PyObject* daff_close( PyObject*, PyObject** ppArgs, Py_ssize_t nArgs, PyO
 
 static PyObject* daff_nearest_neighbour_index( PyObject*, PyObject** ppArgs, Py_ssize_t nArgs, PyObject* pKeywordNames )
 {
-	// @todo armin
-	return PyLong_FromLong( -1 );
+	
+	// Get the index of the nearest neighbour
+	static const char * const _keywords[] = { "index", "view", "angle1", "angle2", NULL };
+	static _PyArg_Parser _parser = { "iidd:nearest_neighbour_index", _keywords, 0 };
+	int iHandle = -1;
+	int iView;
+	double dAngle1Deg;
+	double dAngle2Deg;
+	
+
+	if( !_PyArg_ParseStack( ppArgs, nArgs, pKeywordNames, &_parser, &iHandle, &iView, &dAngle1Deg, &dAngle2Deg) )
+		return NULL;
+
+	if( !ValidHandle( iHandle ) )
+	{
+		PyErr_SetString( PyExc_ConnectionError, std::string( "Invalid DAFF handle" ).c_str() );
+		return NULL;
+	}
+	else
+	{
+		DAFFReader* pReader = g_mReader[ iHandle ];
+		int iRecordIndex;
+		bool bOutOfBounds;
+
+		pReader->getContent()->getNearestNeighbour(iView, dAngle1Deg, dAngle2Deg, iRecordIndex, bOutOfBounds );
+
+		//Python list which contains the Record index and the OutOfBounds Indicator
+		PyObject* pyNearestNeighbour = PyList_New(0);
+		PyList_Append(pyNearestNeighbour, PyLong_FromLong(iRecordIndex));
+		PyList_Append(pyNearestNeighbour, PyBool_FromLong(bOutOfBounds));
+
+
+		//Return Python list
+		return pyNearestNeighbour;
+	}
+	
+	return Py_None;
 }
 
 static PyObject* daff_nearest_neighbour_record( PyObject*, PyObject** ppArgs, Py_ssize_t nArgs, PyObject* pKeywordNames )
 {
-	// @todo armin, compare VA struct in VAPython
-	return Py_None;
+	// Get the record of the nearest neighbour
+	static const char * const _keywords[] = { "index", "view", "angle1", "angle2", NULL };
+	static _PyArg_Parser _parser = { "iidd:nearest_neighbour_index", _keywords, 0 };
+	int iHandle = -1;
+	int iView;
+	double dAngle1Deg;
+	double dAngle2Deg;
+
+	// Initialize a new list
+	PyObject* record = PyList_New(0);
+
+	if (!_PyArg_ParseStack(ppArgs, nArgs, pKeywordNames, &_parser, &iHandle, &iView, &dAngle1Deg, &dAngle2Deg))
+		return NULL;
+
+	if (!ValidHandle(iHandle))
+	{
+		PyErr_SetString(PyExc_ConnectionError, std::string("Invalid DAFF handle").c_str());
+		return NULL;
+	}
+	else
+	{
+		DAFFReader* pReader = g_mReader[iHandle];
+		int iRecordIndex;
+		pReader->getContent()->getNearestNeighbour(iView, dAngle1Deg, dAngle2Deg, iRecordIndex);
+
+		//Get the record and set the record list
+		GetRecordPython(pReader, iRecordIndex, record);
+
+		//Return the record of the nearest neighbour
+		return record;
+
+	}
+	
+	return record;
 }
+
+// Fetch a record from the content, convert and return it as a python list
+static void GetRecordPython(DAFFReader* pReader, int iRecordIndex, PyObject* &listResult)
+{
+	int iContentType = pReader->getContentType();
+	int iChannels = pReader->getProperties()->getNumberOfChannels();
+
+	// Create the result 
+	listResult = PyList_New(0);
+	
+	if (iContentType == DAFF_IMPULSE_RESPONSE)
+	{
+		DAFFContentIR* pContent = dynamic_cast< DAFFContentIR* >(pReader->getContent());
+		int iFilterLength = pContent->getFilterLength();
+
+
+		// Copy the data
+		for (int c = 0; c < iChannels; c++)
+		{
+			//Create python list for current channel
+			PyObject* listChannel = PyList_New(0);
+
+			// Get the impulse response
+			float* fImpulseResponse;
+			pContent->getFilterCoeffs(iRecordIndex, c, fImpulseResponse);
+
+			// Append it to current channel list
+			for (int i = 0; i < iFilterLength; i++)
+				PyList_Append(listChannel, PyFloat_FromDouble(fImpulseResponse[i]));
+
+			// Append channel List to result List
+			PyList_Append(listResult, listChannel);
+
+		}
+	}
+
+	if (iContentType == DAFF_MAGNITUDE_SPECTRUM)
+	{
+		DAFFContentMS* pContent = dynamic_cast< DAFFContentMS* >(pReader->getContent());
+		int iNumFreqs = pContent->getNumFrequencies();
+
+
+		// Copy the data
+		for (int c = 0; c < iChannels; c++) 
+		{
+			// Create python list for current channel
+			PyObject* listChannel = PyList_New(0);
+
+			// Get the magnitude spectrum
+			float* fMagnitudes;
+			pContent->getMagnitudes(iRecordIndex, c, fMagnitudes);
+
+			// Append it to current channel list
+			for (int i = 0; i < iNumFreqs; i++)
+				PyList_Append(listChannel, PyFloat_FromDouble(fMagnitudes[i]));
+
+			// Append channel List to result List
+			PyList_Append(listResult, listChannel);
+		}
+	}
+
+	if (iContentType == DAFF_PHASE_SPECTRUM)
+	{
+		DAFFContentPS* pContent = dynamic_cast< DAFFContentPS* >(pReader->getContent());
+		int iNumFreqs = pContent->getNumFrequencies();
+
+
+		// Copy the data
+		for (int c = 0; c < iChannels; c++)
+		{
+			//Create python list for current channel
+			PyObject* listChannel = PyList_New(0);
+
+			// Get the phase coefficients
+			float* fPhases;
+			pContent->getPhases(iRecordIndex, c, fPhases);
+
+			// Append it to current channel list
+			for (int i = 0; i < iNumFreqs; i++)
+				PyList_Append(listChannel, PyFloat_FromDouble(fPhases[i]));
+
+			// Append channel List to result List
+			PyList_Append(listResult, listChannel);
+		}
+	}
+
+	if (iContentType == DAFF_MAGNITUDE_PHASE_SPECTRUM)
+	{
+		DAFFContentMPS* pContent = dynamic_cast< DAFFContentMPS* >(pReader->getContent());
+		int iNumFreqs = pContent->getNumFrequencies();
+
+
+		// Copy the data
+		for (int c = 0; c < iChannels; c++)
+		{
+			//Create python list for current channel
+			PyObject* listChannel = PyList_New(0);
+
+			// Get the impulse response
+			float* fCoeffs;
+			pContent->getCoefficientsRI(iRecordIndex, c, fCoeffs);
+
+			// Append it to current channel list. Even indexes are real and odd indexes are imaginary.
+			for (int i = 0; i < iNumFreqs; i++) 
+			{
+				PyList_Append(listChannel, PyComplex_FromDoubles(fCoeffs[2*i], fCoeffs[2*i+1]));
+			}
+
+			// Append channel List to result List
+			PyList_Append(listResult, listChannel);
+		}
+	}
+
+	if (iContentType == DAFF_DFT_SPECTRUM)
+	{
+		DAFFContentDFT* pContent = dynamic_cast< DAFFContentDFT* >(pReader->getContent());
+		int iNumDFTCoeffs = pContent->getNumDFTCoeffs();
+
+
+		// Copy the data
+		for (int c = 0; c < iChannels; c++)
+		{
+			//Create python list for current channel
+			PyObject* listChannel = PyList_New(0);
+
+			// Get the impulse response
+			float* fDFTCoeffs;
+			pContent->getDFTCoeffs(iRecordIndex, c, fDFTCoeffs);
+
+			// Append it to current channel list. Even indexes are real and odd indexes are imaginary.
+			for (int i = 0; i < iNumDFTCoeffs; i++)
+			{
+				PyList_Append(listChannel, PyComplex_FromDoubles(fDFTCoeffs[2 * i], fDFTCoeffs[2 * i + 1]));
+			}
+
+			// Append channel List to result List
+			PyList_Append(listResult, listChannel);
+		}
+	}
+}
+
 
 static PyObject* daff_content_type( PyObject*, PyObject** ppArgs, Py_ssize_t nArgs, PyObject* pKeywordNames )
 {
-	static const char * const _keywords[] = { "content_type", NULL };
+	static const char * const _keywords[] = { "index", NULL };
 	static _PyArg_Parser _parser = { "i:content_type", _keywords, 0 };
 	int iHandle = -1;
 
@@ -129,17 +340,275 @@ static PyObject* daff_content_type( PyObject*, PyObject** ppArgs, Py_ssize_t nAr
 	else
 	{
 		DAFFReader* pReader = g_mReader[ iHandle ];
-		int iContentType = pReader->getContentType();
+		int iContentType = pReader->getContentType(); 
 		return PyLong_FromLong( iContentType );
 	}
 }
 
-
-static PyObject* daff_dummy( PyObject*, PyObject** ppArgs, Py_ssize_t nArgs, PyObject* pKeywordNames )
+static PyObject* daff_content_type_str(PyObject*, PyObject** ppArgs, Py_ssize_t nArgs, PyObject* pKeywordNames)
 {
-	// @todo armin, remove when unused
+	static const char * const _keywords[] = { "index", NULL };
+	static _PyArg_Parser _parser = { "i:content_type_str", _keywords, 0 };
+	int iHandle = -1;
+
+	if (!_PyArg_ParseStack(ppArgs, nArgs, pKeywordNames, &_parser, &iHandle))
+		return NULL;
+
+	if (!ValidHandle(iHandle))
+	{
+		PyErr_SetString(PyExc_ConnectionError, std::string("Invalid DAFF handle").c_str());
+		return NULL;
+	}
+	else
+	{
+		DAFFReader* pReader = g_mReader[iHandle];
+		std::string sContentType = DAFFUtils::StrContentType(pReader->getContentType());
+		return PyUnicode_FromString(sContentType.c_str());
+	}
+}
+
+static PyObject* daff_metadata(PyObject*, PyObject** ppArgs, Py_ssize_t nArgs, PyObject* pKeywordNames)
+{
+	static const char * const _keywords[] = { "index", NULL };
+	static _PyArg_Parser _parser = { "i:content_type_str", _keywords, 0 };
+	int iHandle = -1;
+
+	if (!_PyArg_ParseStack(ppArgs, nArgs, pKeywordNames, &_parser, &iHandle))
+	{
+		PyErr_SetString(PyExc_KeyError, std::string("Wrong number of arguments").c_str());
+		return NULL;
+	}
+
+	if (!ValidHandle(iHandle))
+	{
+		PyErr_SetString(PyExc_ConnectionError, std::string("Invalid DAFF handle").c_str());
+		return NULL;
+	}
+	else
+	{
+
+		DAFFReader* pReader = g_mReader[iHandle];
+		int iContentType = pReader->getContentType();
+
+		//Create a new python dictionary
+		PyObject* metadata = PyDict_New();
+
+		//Get the metadata
+		const DAFFMetadata* pMetadata = pReader->getMetadata();
+
+		// Get all the metadata keys
+		std::vector< std::string > vsKeys;
+		pMetadata->getKeys(vsKeys);
+
+
+		for each (std::string sKey in vsKeys)
+		{
+			//Current key as python object
+			PyObject* pyKey = PyUnicode_FromString(sKey.c_str());
+
+			//Check the value type and set the value to the python dictionary at the current key
+			switch (pMetadata->getKeyType(sKey))
+			{
+			case DAFFMetadata::DAFF_BOOL:
+				//Set the boolean value
+				PyDict_SetItem(metadata, pyKey, PyBool_FromLong(pMetadata->getKeyBool(sKey)));
+				break;
+
+			case DAFFMetadata::DAFF_INT:
+				//Set the integer value
+				PyDict_SetItem(metadata, pyKey, PyLong_FromLong(pMetadata->getKeyInt(sKey)));
+				break;
+
+			case DAFFMetadata::DAFF_FLOAT:
+				//Set the floating point value
+				PyDict_SetItem(metadata, pyKey, PyFloat_FromDouble(pMetadata->getKeyFloat(sKey)));
+				break;
+
+			case DAFFMetadata::DAFF_STRING:
+				//Set the string value
+				PyDict_SetItem(metadata, pyKey, PyUnicode_FromString(pMetadata->getKeyString(sKey).c_str()));
+				break;
+			}
+			
+		}
+		
+		return metadata;
+	}
+
+	
+	PyErr_SetString(PyExc_ConnectionError, std::string("Something went wrong.").c_str());
 	return Py_None;
 }
+
+static PyObject* daff_properties(PyObject*, PyObject** ppArgs, Py_ssize_t nArgs, PyObject* pKeywordNames)
+{
+	static const char * const _keywords[] = { "index", NULL };
+	static _PyArg_Parser _parser = { "i:content_type_str", _keywords, 0 };
+	int iHandle = -1;
+
+	if (!_PyArg_ParseStack(ppArgs, nArgs, pKeywordNames, &_parser, &iHandle))
+	{
+		PyErr_SetString(PyExc_KeyError, std::string("Wrong number of arguments").c_str());
+		return NULL;
+	}
+
+	if (!ValidHandle(iHandle))
+	{
+		PyErr_SetString(PyExc_ConnectionError, std::string("Invalid DAFF handle").c_str());
+		return NULL;
+	}
+	else
+	{
+
+		//Create a new python dictionary
+		PyObject* pyProperties = PyDict_New();
+
+
+		DAFFReader* pReader = g_mReader[iHandle];
+		const DAFFProperties* pProps = pReader->getProperties();
+
+		// --= Create a Python dictionary of the property fields =----------
+
+		// Filename
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("filename"), PyUnicode_FromString(pReader->getFilename().c_str()));
+
+		// File format version
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("fileFormatVersion"), PyLong_FromLong(pReader->getFileFormatVersion()));
+
+		// Content type
+		int iContentType = pReader->getContentType();
+		std::string sContentType = DAFFUtils::StrShortContentType(iContentType);
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("contentType"), PyUnicode_FromString(sContentType.c_str()));
+
+		// Quantization
+		int iQuantization = pProps->getQuantization();
+		std::string sQuantization;
+		if (iQuantization == DAFF_INT16)
+			sQuantization = "int16";
+		if (iQuantization == DAFF_INT24)
+			sQuantization = "int24";
+		if (iQuantization == DAFF_FLOAT32)
+			sQuantization = "float32";
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("quantization"), PyUnicode_FromString(sQuantization.c_str()));
+
+		// Number of channels
+		int iChannels = pProps->getNumberOfChannels();
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("numChannels"), PyLong_FromLong(iChannels));
+
+		// Number of records
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("numRecords"), PyLong_FromLong(pProps->getNumberOfRecords()));
+
+		// Channel labels (python list of strings)
+		PyObject* pyChannelLabels = PyList_New(0);
+		for (int c = 0; c < iChannels; c++)
+		{
+			if (!pProps->getChannelLabel(c).empty())
+				PyList_Append(pyChannelLabels, PyUnicode_FromString(pProps->getChannelLabel(c).c_str()));
+		}
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("channelLabels"), pyChannelLabels);
+
+
+		// Alpha points 
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("alphaPoints"), PyLong_FromLong(pProps->getAlphaPoints()));
+
+		// Alpha resolution
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("alphaResolution"), PyFloat_FromDouble(pProps->getAlphaResolution()));
+
+		// Alpha range
+		PyObject* pyAlphaRange = PyList_New(0);
+		float alphaStart = pProps->getAlphaStart();
+		float alphaEnd = pProps->getAlphaEnd();
+		PyList_Append(pyAlphaRange, PyFloat_FromDouble(alphaStart));
+		PyList_Append(pyAlphaRange, PyFloat_FromDouble(alphaEnd));
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("alphaRange"), pyAlphaRange);
+
+		// Beta points 
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("betaPoints"), PyLong_FromLong(pProps->getBetaPoints()));
+
+		// Beta resolution
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("betaResolution"), PyFloat_FromDouble(pProps->getBetaResolution()));
+
+		// Beta range
+		PyObject* pyBetaRange = PyList_New(0);
+		float betaStart = pProps->getBetaStart();
+		float betaEnd = pProps->getBetaEnd();
+		PyList_Append(pyBetaRange, PyFloat_FromDouble(betaStart));
+		PyList_Append(pyBetaRange, PyFloat_FromDouble(betaEnd));
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("betaRange"), pyBetaRange);
+
+		// Orientation
+		DAFFOrientationYPR orient;
+		PyObject* pyOrientation = PyDict_New();
+		pProps->getOrientation(orient);
+		float fYawAngle = orient.fYawAngleDeg;
+		float fPitchAngle = orient.fPitchAngleDeg;
+		float fRollAngle = orient.fRollAngleDeg;
+		PyDict_SetItem(pyOrientation, PyUnicode_FromString("yawAngle"), PyFloat_FromDouble(fYawAngle));
+		PyDict_SetItem(pyOrientation, PyUnicode_FromString("pitchAngle"), PyFloat_FromDouble(fPitchAngle));
+		PyDict_SetItem(pyOrientation, PyUnicode_FromString("rollAngle"), PyFloat_FromDouble(fRollAngle));
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("orientation"), pyOrientation);
+
+		// Default orientation
+		PyObject* pyDefOrientation = PyDict_New();
+
+		pProps->getDefaultOrientation(orient);
+		fYawAngle = orient.fYawAngleDeg;
+		fPitchAngle = orient.fPitchAngleDeg;
+		fRollAngle = orient.fRollAngleDeg;
+		PyDict_SetItem(pyDefOrientation, PyUnicode_FromString("yawAngle"), PyFloat_FromDouble(fYawAngle));
+		PyDict_SetItem(pyDefOrientation, PyUnicode_FromString("pitchAngle"), PyFloat_FromDouble(fPitchAngle));
+		PyDict_SetItem(pyDefOrientation, PyUnicode_FromString("rollAngle"), PyFloat_FromDouble(fRollAngle));
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("orientationDefault"), pyDefOrientation);
+
+
+		// Full sphere
+		PyDict_SetItem(pyProperties, PyUnicode_FromString("fullSphere"), PyBool_FromLong(pProps->coversFullSphere()));
+		
+		if (iContentType == DAFF_IMPULSE_RESPONSE)
+		{
+			DAFFContentIR* pContent = dynamic_cast< DAFFContentIR* >(pReader->getContent());
+
+			// Samplerate
+			PyDict_SetItem(pyProperties, PyUnicode_FromString("samplerate"), PyFloat_FromDouble(pContent->getSamplerate()));
+
+			// Filter length
+			PyDict_SetItem(pyProperties, PyUnicode_FromString("filterLength"), PyLong_FromLong(pContent->getFilterLength()));
+		}
+		else if (iContentType == DAFF_MAGNITUDE_SPECTRUM || iContentType == DAFF_PHASE_SPECTRUM || iContentType == DAFF_MAGNITUDE_PHASE_SPECTRUM)
+		{
+			DAFFContentMS* pContent = dynamic_cast< DAFFContentMS* >(pReader->getContent());
+
+			// Number of frequencies
+			int iNumFreqs = pContent->getNumFrequencies();
+			PyDict_SetItem(pyProperties, PyUnicode_FromString("numFreqs"), PyLong_FromLong(iNumFreqs));
+
+			// Frequencies
+			PyObject* pyFrequencies = PyList_New(0);
+			const std::vector<float>& vfFreqs = pContent->getFrequencies();
+			for (int i = 0; i < iNumFreqs; i++)
+				PyList_Append(pyFrequencies, PyFloat_FromDouble(vfFreqs[i]));
+			PyDict_SetItem(pyProperties, PyUnicode_FromString("freqs"), pyFrequencies);
+		}
+		else if (iContentType == DAFF_DFT_SPECTRUM) 
+		{
+			DAFFContentDFT* pContent = dynamic_cast< DAFFContentDFT* >(pReader->getContent());
+
+			PyDict_SetItem(pyProperties, PyUnicode_FromString("transformSize"), PyLong_FromLong(pContent->getTransformSize()));
+			PyDict_SetItem(pyProperties, PyUnicode_FromString("numDFTCoeffs"), PyLong_FromLong(pContent->getNumDFTCoeffs()));
+			PyDict_SetItem(pyProperties, PyUnicode_FromString("isSymmetric"), PyBool_FromLong(pContent->isSymmetric()));
+			PyDict_SetItem(pyProperties, PyUnicode_FromString("samplerate"), PyFloat_FromDouble(pContent->getSamplerate()));
+			PyDict_SetItem(pyProperties, PyUnicode_FromString("frequencyBandwidth"), PyFloat_FromDouble(pContent->getFrequencyBandwidth()));
+		}
+
+
+		return pyProperties;
+	}
+
+
+	PyErr_SetString(PyExc_ConnectionError, std::string("Something went wrong.").c_str());
+	return Py_None;
+}
+
 
 // ------------------ module definitions ------------------ //
 
@@ -150,10 +619,9 @@ static PyMethodDef daff_methods[] =
 	{ "nearest_neighbour_index", ( PyCFunction ) daff_nearest_neighbour_index, METH_FASTCALL, no_doc },
 	{ "nearest_neighbour_record", ( PyCFunction ) daff_nearest_neighbour_record, METH_FASTCALL, no_doc },
 	{ "content_type", ( PyCFunction ) daff_content_type, METH_FASTCALL, no_doc },
-	{ "content_type_str", ( PyCFunction ) daff_dummy, METH_FASTCALL, no_doc },
-	{ "metadata", ( PyCFunction ) daff_dummy, METH_FASTCALL, no_doc },
-	{ "properties", ( PyCFunction ) daff_dummy, METH_FASTCALL, no_doc },
-	{ "metadata", ( PyCFunction ) daff_dummy, METH_FASTCALL, no_doc },
+	{ "content_type_str", ( PyCFunction ) daff_content_type_str, METH_FASTCALL, no_doc },
+	{ "metadata", ( PyCFunction ) daff_metadata, METH_FASTCALL, no_doc },
+	{ "properties", ( PyCFunction ) daff_properties, METH_FASTCALL, no_doc },
 	{ NULL, NULL, 0, NULL }        /* Sentinel */
 };
 
