561564ac5068f91302a9f2a7bc4a17de21df32c4
jstienen <jstienen@22735341-649f-4b50-8b46-d246c0249a88>
2018-06-29 06:12:02 +0000

    Applying changes that have been committed to ITA toolbox repository
    git-svn-id: https://svn.code.sf.net/p/opendaff/code/trunk@379 22735341-649f-4b50-8b46-d246c0249a88

--- matlab/hrtfs/TUB/example_convert_FABIAN_HATO.m
+++ matlab/hrtfs/TUB/example_convert_FABIAN_HATO.m
@@ -2,6 +2,8 @@
 % Resources
 % http://dx.doi.org/10.14279/depositonce-5718.2
 % http://www.ak.tu-berlin.de/menue/digitale_ressourcen/research_tools/aktools/
+addpath( genpath( 'AKtools' ) )
+addpath( genpath( 'FABIAN_HRTF_DATABASE_V2' ) )
 AKdependencies('FABIAN')
 
 %% Configure
@@ -11,7 +13,9 @@ export_properties.alpharange = [0 360];
 export_properties.betares = 5;
 export_properties.betarange = [0 180];
 export_properties.hatores = 5;
-export_properties.hatorange = [-40 40];
+export_properties.hatorange = [-50 50];
+export_properties.reference = 'torso'; % the head-above-torso angle rotates the head above torso [default FABIAN reference]
+
 
 %% Set up metadata
 additional_metadata = daffv17_add_metadata( [], 'hato', 'BOOL', true );
@@ -22,5 +26,11 @@ additional_metadata = daffv17_add_metadata( additional_metadata, 'AKtools_resour
 additional_metadata = daffv17_add_metadata( additional_metadata, 'FABIAN_resource', 'STRING', 'http://dx.doi.org/10.14279/depositonce-5718.2' );
 additional_metadata = daffv17_add_metadata( additional_metadata, 'FABIAN_license', 'STRING', 'Creative Commons BY-NC-SA 4.0' );
 
-%% Export
-daffv17_convert_from_aktools( 'FABIAN_HATO_5x5x5_256_44100Hz.v17.ir.daff', export_properties, additional_metadata )
+%% Export untouched
+additional_metadata_untouched = daffv17_add_metadata( additional_metadata, 'reference', 'STRING', export_properties.reference );
+daffv17_convert_from_aktools( 'FABIAN_HATO_5x5x5_256_44100Hz_debug.v17.ir.daff', export_properties, additional_metadata_untouched )
+
+%% Export for VA with head as reference frame
+export_properties.reference = 'head'; % the head-above-torso angle rotates torso not head (for Virtual Acoustics)
+additional_metadata_va = daffv17_add_metadata( additional_metadata, 'reference', 'STRING', export_properties.reference );
+daffv17_convert_from_aktools( 'FABIAN_OTAH_5x5x5_256_44100Hz_debug.v17.ir.daff', export_properties, additional_metadata_va )
